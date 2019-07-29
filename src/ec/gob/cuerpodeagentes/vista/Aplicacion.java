bb593caf9f3c64a9a66840abf582c9385d78f06f
luiscatota001 <luiscatota001@gmail.com>
2019-07-29 09:56:41 -0500

    Version inicial de fuguras geometricas

---
+++ src/ec/gob/cuerpodeagentes/vista/Aplicacion.java
@@ -0,0 +1,14 @@
+
+package ec.gob.cuerpodeagentes.vista;
+
+import ec.gob.cuerpodeagentes.controlador.Controlador;
+
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Controlador controlador=new Controlador();
+        controlador.menu();
+        
+    }
+    
+}
